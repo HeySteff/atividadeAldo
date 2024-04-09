4dfeec636330866f496935856ce78681a8a68b27
Stefano Matheus <Stefanomatheus@hotmail.com>
2024-04-09 19:21:05 -0300

    Create ContribuicaoSindical2.java

---
+++ src/atividadeAldo/ContribuicaoSindical2.java
@@ -0,0 +1,42 @@
+package atividadeAldo;
+
+	import java.util.Scanner;
+
+public class ContribuicaoSindical2 {
+
+	public static void main(String[] args) {
+		
+        Scanner scanner = new Scanner(System.in);
+
+        final double salarioMinimo = 1212.00;
+
+        double salarioBruto = 0.0;
+        int tipoContribuicao = -1;
+
+        do {
+            System.out.print("Digite seu salário bruto: ");
+            salarioBruto = scanner.nextDouble();
+            if (salarioBruto < salarioMinimo) {
+                System.err.println("Salário bruto inválido! Deve ser igual ou superior ao salário mínimo (" + salarioMinimo + ").");
+            }
+        } while (salarioBruto < salarioMinimo);
+
+        do {
+            System.out.print("Digite o tipo de contribuição sindical (0 - não contribuinte, 1 - contribuinte): ");
+            tipoContribuicao = scanner.nextInt();
+            if (tipoContribuicao != 0 && tipoContribuicao != 1) {
+                System.err.println("Tipo de contribuição sindical inválido! Deve ser 0 ou 1.");
+            }
+        } while (tipoContribuicao != 0 && tipoContribuicao != 1);
+
+        double valorContribuicaoSindical = 0.0;
+        if (tipoContribuicao == 1) {
+            valorContribuicaoSindical = salarioBruto / 30.0;
+        }
+
+        double salarioLiquido = salarioBruto - valorContribuicaoSindical;
+
+        System.out.println("Valor da contribuição sindical: R$" + String.format("%.2f", valorContribuicaoSindical));
+        System.out.println("Salário líquido: R$" + String.format("%.2f", salarioLiquido));
+    }
+}
