c4855e3acdc65c89d87803bb94acee7a8de54e14
stefano <stefano.matheus@souunit.com.br>
2024-06-02 18:41:14 -0300

    Update Validacao_De_Matricula.java

--- src/atividadeAldo/Validacao_De_Matricula.java
+++ src/atividadeAldo/Validacao_De_Matricula.java
@@ -1,15 +1,55 @@
 package atividadeAldo;
 
+import java.util.Scanner;
+
 public class Validacao_De_Matricula {
 
 	public static void main(String[] args) {
 
-		String nome = "Eu Programando";
-		
-		System.out.println(nome.substring(3));
-		System.out.println(nome.substring(3, 11));
-		System.out.println(nome.substring(3, nome.length(4)));
-		System.out.println(nome);
-		
+		Scanner in = new Scanner(System.in);
+		String matricula, ano, matriculaAluno, anoAluno, nomeCurso, curso;
+		char semestre;
+
+		do {
+
+			System.out.println("informe a matricula Exemplo(CAAAA.S.99999)");
+			matricula = in.nextLine().toUpperCase();
+
+			if (matricula.length() != 13) {
+
+				System.out.println("Matricula informada errada");
+			}
+
+		} while (matricula.length() != 13);
+
+		curso = matricula.substring(0, 1);
+		if (curso != "A" && curso != "B" && curso != "C") {
+			System.out.println("Letra do curso informado errado, use o Exemplo:(CAAAA.S.99999)");
+		}
+
+		semestre = matricula.charAt(6);
+		if (semestre != '1' && semestre != '2') {
+			System.out.println("Semestre do curso informado errado, use o Exemplo:(CAAAA.S.99999)");
+		}
+
+		if (matricula.charAt(5) != '.' || matricula.charAt(7) != '.') {
+			System.out.println("os Pontos (.) não estão na posição correta, se o Exemplo:(CAAAA.S.99999)");
+		}
+
+		if (curso.equals("A")) {
+			nomeCurso = "Administração";
+		} else if (curso.equals("B")) {
+			nomeCurso = "Direito";
+		} else if (curso.equals("C")) {
+			nomeCurso = "Nutrição";
+		} else {
+//			throw new IllegalStateException("Curso inválido: " + curso); // Exceção para garantir consistência
+			return;
+		}
+		matriculaAluno = matricula.substring(8);
+		anoAluno = matricula.substring(1, 5);
+
+		System.out.printf("Aluno %s do curso de %s, matrícula do ano %s do %sº semestre%n", matriculaAluno, nomeCurso,
+				anoAluno, semestre);
 	}
 }
