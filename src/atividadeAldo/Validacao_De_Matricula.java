2fdabd836a5da09235ffc4a5eab40c70d380fe3b
stefano <stefano.matheus@souunit.com.br>
2024-06-04 09:36:19 -0300

    Correção do looping e da verificação de letras

--- src/atividadeAldo/Validacao_De_Matricula.java
+++ src/atividadeAldo/Validacao_De_Matricula.java
@@ -20,35 +20,36 @@ public class Validacao_De_Matricula {
 				System.out.println("Matricula informada errada");
 			}
 
-		} while (matricula.length() != 13);
+			curso = matricula.substring(0, 1);
+			if (!curso.equals("A") && !curso.equals("B") && !curso.equals("C")) {
+				System.out.println("Letra do curso informado errado, use o Exemplo:(CAAAA.S.99999)");
+			}
 
-		curso = matricula.substring(0, 1);
-		if (curso != "A" && curso != "B" && curso != "C") {
-			System.out.println("Letra do curso informado errado, use o Exemplo:(CAAAA.S.99999)");
-		}
+			semestre = matricula.charAt(6);
+			if (semestre != '1' && semestre != '2') {
+				System.out.println("Semestre do curso informado errado, use o Exemplo:(CAAAA.S.99999)");
+			}
 
-		semestre = matricula.charAt(6);
-		if (semestre != '1' && semestre != '2') {
-			System.out.println("Semestre do curso informado errado, use o Exemplo:(CAAAA.S.99999)");
-		}
+			if (matricula.charAt(5) != '.' || matricula.charAt(7) != '.') {
+				System.out.println("Os pontos (.) não estão na posição correta, use o Exemplo:(CAAAA.S.99999)");
+			}
 
-		if (matricula.charAt(5) != '.' || matricula.charAt(7) != '.') {
-			System.out.println("os Pontos (.) não estão na posição correta, se o Exemplo:(CAAAA.S.99999)");
-		}
+		} while (matricula.length() != 13 || !curso.equals("A") && !curso.equals("B") && !curso.equals("C")
+				|| semestre != '1' && semestre != '2' || matricula.charAt(5) != '.' || matricula.charAt(7) != '.');
 
 		if (curso.equals("A")) {
 			nomeCurso = "Administração";
 		} else if (curso.equals("B")) {
 			nomeCurso = "Direito";
-		} else if (curso.equals("C")) {
-			nomeCurso = "Nutrição";
 		} else {
-			return;
+			nomeCurso = "Nutrição";
 		}
 		matriculaAluno = matricula.substring(8);
 		anoAluno = matricula.substring(1, 5);
 
 		System.out.printf("Aluno %s do curso de %s, matrícula do ano %s do %sº semestre%n", matriculaAluno, nomeCurso,
 				anoAluno, semestre);
+
+		// in.close();
 	}
 }
